248637919f02961d511868a7e5279b28620d9b9b
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-12-03 13:41:24 +0100

    Added user_cmd check limits

--- work_hours.py
+++ work_hours.py
@@ -1,5 +1,10 @@
 import sys
 
+def user_cmd_not_ok(cmd):
+    if cmd < 0 or cmd > 2:
+        return True
+    else:
+        return False
 
 while True:
     print()
@@ -9,12 +14,14 @@ while True:
     print("[2] - Zakonczenie pracy")
 
     user_cmd = int(input("Twoj wybor: "))
+    if user_cmd_not_ok(user_cmd):
+        print("Nieprawdilowy wybor!")
+    else:
+        if user_cmd == 0:
+            exit(0)
 
-    if user_cmd == 0:
-        exit(0)
+        if user_cmd == 1:
+            print("Praca rozpoczeta")
 
-    if user_cmd == 1:
-        print("Praca rozpoczeta")
-
-    if user_cmd == 2:
-        print('Praca zakonczona')
+        if user_cmd == 2:
+            print('Praca zakonczona')
